d5819bf2645627d6152c45b4296d9cca61fd177f
Akanksha Bhatt <89566429+Akanksha-bhatt-aspirenxt@users.noreply.github.com>
2021-12-09 02:37:01 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,9 +1,3 @@
-# After creating Admin_workspace bundle id will be Added to the fleet of workspaces
-
-data "aws_workspaces_bundle" "custom_windows_10" {
-  bundle_id = var.custom_bundle # Value with Windows 10 (English)
-}
-
 data "aws_workspaces_bundle" "admin_windows_10" {
   bundle_id = var.admin_bundle_id # Value with Windows 10 (English)
 }
