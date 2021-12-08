dc3d5b79037da53542c910f3843abd530a784751
Akanksha Bhatt <89566429+Akanksha-bhatt-aspirenxt@users.noreply.github.com>
2021-12-09 02:15:07 +0530

    Update and rename workspaces_fleet.tf.disabled to workspaces_fleet.tf

--- workspaces_fleet.tf
+++ workspaces_fleet.tf
@@ -6,7 +6,7 @@ module "fleet_workspaces" {
   directory_id          = aws_workspaces_directory.this.id
   bundle_id             = data.aws_workspaces_bundle.custom_windows_10.id # Windows 10 
   user_name             = "${element(var.workspaces_usernames, count.index)}"
-  volume_encryption_key = var.volume_encryption_key
+  
 
   #workspace_properties
 
