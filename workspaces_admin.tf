08b5bceec2a0095cbcb005d8a4abd829d3c42aa9
Akanksha Bhatt <89566429+Akanksha-bhatt-aspirenxt@users.noreply.github.com>
2021-12-09 03:28:17 +0530

    Update workspaces_admin.tf

--- workspaces_admin.tf
+++ workspaces_admin.tf
@@ -5,7 +5,7 @@ module "admin_workspace" {
   directory_id          = aws_workspaces_directory.this.id
   bundle_id             = data.aws_workspaces_bundle.admin_windows_10.id 
   user_name             = var.admin_workspace_username
- # volume_encryption_key = var.volume_encryption_key # KMS Key
+ 
 
   #workspace_properties
 
